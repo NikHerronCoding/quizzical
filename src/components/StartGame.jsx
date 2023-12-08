2065dfc3669c92a6615a84e7169475eacef06b6b
Nik <nherron123@gmail.com>
2023-12-08 16:33:04 -0700

    added better mobile functionality css

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -4,7 +4,7 @@ export default function StartGame(props) {
     return (
         <>
             <h1>Quizzical</h1>
-            <h3>A quiz game written in React and based on the OpenTrivia API</h3>
+            <h3>A quiz game written in React, based on the OpenTrivia API</h3>
             <button onClick={props.click}>
                 Start Quiz
             </button>
